4eaaeee2953b96b824f5920b05702b2f1d126e48
Alex Synelnyk <xalev@ukr.net>
2025-03-22 14:06:46 +0200

    Initial Commit

---
+++ FlexMusic/FlexMusicApp.swift
@@ -0,0 +1,20 @@
+//
+//  FlexMusicApp.swift
+//  FlexMusic
+//
+//  Created by Aleks Synelnyk on 22.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct FlexMusicApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
